9efcd29537e55a4184c5a53e840d16ee656b0989
NagaSadow <62349423+NagaSadow@users.noreply.github.com>
2021-01-07 16:43:40 +0100

    Add files via upload

---
+++ Lab7/watki.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+
+int main(){
+
+  int ilosc_watkow = 3;
+  pthread_t id_watkow[ilosc_watkow];
+
+
+  void *funkcja_watku(void *id){
+
+    int idw = (int) id;
+    printf("Hello SCR. Written by thread %d \n", idw);
+    pthread_exit(NULL);
+  }
+
+  for(int k = 0; k < ilosc_watkow; k++){
+
+    int tmp = pthread_create(&id_watkow[k], NULL, funkcja_watku, (void*) id_watkow[k]);
+  }
+
+  pthread_exit(NULL);
+}
